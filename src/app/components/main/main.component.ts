53bc9cd360a766e9b954bea91c2c066c3701a9c3
Catherine French <catherine.french@me.com>
2019-10-27 12:31:25 -0400

    Adds property class and FormsModule

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from '../../property';
 
 @Component({
   selector: 'app-main',
@@ -13,3 +14,7 @@ export class MainComponent implements OnInit {
   }
 
 }
+let myProperty =  new Property('23 Cannon Place', 'Ashridge Fine Foods',
+                       305000,
+                       63500);
+console.log('My property is' + myProperty.name); // "My hero is called SkyDog"
---
+++ src/app/property.ts
@@ -0,0 +1,10 @@
+export class Property {
+	constructor(
+		public name: string,
+		public tennantName?: string,
+		public rentableArea: number,
+		public tenantArea: number
+	) {}
+}
+
+
